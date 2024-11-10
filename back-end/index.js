43da98a5148a264321abc23fded7a5640565f2c7
khaled gamal <khgamal005@gmail.com>
2024-11-11 00:19:34 +0200

    add

--- back-end/index.js
+++ back-end/index.js
@@ -4,30 +4,10 @@ const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
-const multer = require('multer');
 
 
 
-const app = express()
-// CORS Configuration
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:5173']; // Add all allowed origins here
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps, Postman, etc.)
-    if (!origin) return callback(null, true);
-
-    // Check if the origin is in the allowed list
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-}));
 
 
 app.use(cors({
